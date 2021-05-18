8ed7fb5e64d33780430384479bd2bad398df6ad0
taran-duba <72531052+taran-duba@users.noreply.github.com>
2021-05-17 21:03:38 -0400

    Add files via upload

---
+++ tv.js
@@ -0,0 +1,39 @@
+img = "";
+status = "";
+objects = [];
+function preload() {
+    img = loadImage("IMG_3896.jpg");
+}
+function setup() {
+    canvas = createCanvas(380, 600);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("button").innerHTML = "Status: Detecting Objects";
+}
+function draw() {
+    image(img, 0, 0, 380, 600);
+    if (status != "") {
+        for (var i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status: Object Detected";
+            document.getElementById("number_of_objects").innerHTML = "Number of Objects Detected: " + objects.length;
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
+function modelLoaded() {
+    console.log("Model Loaded!");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
